c03a1b781d81ab8e902a488a0aa867f58e2fc503
Vinit Gore <vinitgore1234@gmail.com>
2023-07-11 16:12:34 +0530

    Update TestBankAccount.java

--- src/test/java/day02/solved/TestBankAccount.java
+++ src/test/java/day02/solved/TestBankAccount.java
@@ -3,22 +3,40 @@
  */
 package day02.solved;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 /**
  * @author Vinit Gore
  *
  **/
 public class TestBankAccount {
-	 
-    public static void main(String[] args) {
-         
-        //create Account
-        BankAccount acct1 = new BankAccount();
-        acct1.setAccNo("A101");
-        acct1.setName( "Krishna" );
-        acct1.setBalance(1000);
- 
-        System.out.println(acct1.getAccNo());
-        System.out.println(acct1.getName());
-        System.out.println(acct1.getBalance());
-    }
+
+	public static void main(String[] args) {
+
+		// create Account
+		BankAccount acct1 = new BankAccount();
+		acct1.setAccNo("A101");
+		acct1.setName("Krishna");
+		acct1.setBalance(1000);
+		
+		System.out.println(acct1.getAccNo());
+		System.out.println(acct1.getName());
+		System.out.println(acct1.getBalance());
+
+	}
+
+	@Test
+	public void testGetterSetters() {
+		// create Account
+		BankAccount acct1 = new BankAccount();
+		acct1.setAccNo("A101");
+		acct1.setName("Krishna");
+		acct1.setBalance(1000);
+
+		assertEquals("A101", acct1.getAccNo());
+		assertEquals("Krishna", acct1.getName());
+		assertEquals(1000, acct1.getBalance());
+	}
 }
