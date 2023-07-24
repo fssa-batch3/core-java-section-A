1fbdfea41bfe82f5ed99a02506c75f6453a9a1c3
Vinit Gore <vinitgore1234@gmail.com>
2023-07-24 12:05:59 +0530

    Update ConnectionUtil.java

--- src/main/java/day11/solved/ConnectionUtil.java
+++ src/main/java/day11/solved/ConnectionUtil.java
@@ -15,36 +15,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectionUtil {
-	 
-    public static Connection getConnection() throws SQLException  {
- 
-        Connection con = null;
-        String url = "jdbc:mysql://localhost:3306/project";
-        String userName = "root";
-        String passWord = "123456";
-        
-            con = DriverManager.getConnection(url, userName, passWord);
-        
-        return con;
-    }
-     
-    public static void close(Connection conn , Statement stmt, ResultSet rs){
-         
-        try
-        {
-            if ( rs != null ){
-                rs.close();
-            }
-            if ( stmt != null ) {
-                stmt.close();
-            }
-            if ( conn != null ){
-                conn.close();
-            }
-        }
-        catch(SQLException e){
-             e.printStackTrace();
-						 // No need re throw the exception.
-        }
-    }
+
+	public static Connection getConnection() throws SQLException {
+
+		Connection con = null;
+		String url = "jdbc:mysql://localhost:3306/project";
+		String userName = "root";
+		String passWord = "123456";
+
+		con = DriverManager.getConnection(url, userName, passWord);
+
+		return con;
+	}
+
+	public static void close(Connection conn, Statement stmt, ResultSet rs) {
+
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			// No need re throw the exception.
+		}
+	}
 }
